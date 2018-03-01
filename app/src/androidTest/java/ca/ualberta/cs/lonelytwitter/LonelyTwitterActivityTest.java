0ad8d9ea7504750d6119948b97467a239cdebd0b
Echo-Gambino <nkb012pomm@gmail.com>
2018-02-28 17:27:23 -0700

    Thing

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,6 +2,9 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
@@ -10,8 +13,14 @@ import junit.framework.TestCase;
  */
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
 
+    private Solo solo;
+
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void setup() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
     }
 
     public void testStart() throws Exception {
@@ -19,6 +28,20 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
 
     }
 
-    // HELLO
+    public void testTweet() {
+        solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
 
+        solo.enterText((EditText) solo.getView(R.id.body), "test Tweet");
+
+        solo.clickOnButton("Save");
+
+        solo.clearEditText((EditText) solo.getView(R.id.body));
+
+        assertTrue(solo.waitForText("Test Tweet!"));
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,21 +1,11 @@
-/*
- * January 31, 2018
- *
- * Copyright 2018 Hing Yue Lam
- *
- */
-
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -27,19 +17,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.util.Log;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 
 public class LonelyTwitterActivity extends Activity {
 
-	private static final String FILENAME = "newfile.sav";
+	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
 	
-	private ArrayList<Tweet> tweetlist;
-	private ArrayAdapter<Tweet> adapter;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -48,7 +32,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
-		Button clearButton = (Button) findViewById(R.id.clear);
+		Button clearButton = (Button) findViewByID(R.id.clear);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -56,14 +40,8 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-
-				Tweet newtweet = new NormalTweet(text);
-
-				tweetlist.add(newtweet);
-
-				adapter.notifyDataSetChanged();
-
-				saveInFile();
+				saveInFile(text, new Date(System.currentTimeMillis()));
+				// finish();
 
 			}
 		});
@@ -73,10 +51,8 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 
-				tweetlist.clear();
-
+				tweets.clear();
 				adapter.notifyDataSetChanged();
-
 				saveInFile();
 			}
 		});
@@ -87,49 +63,40 @@ public class LonelyTwitterActivity extends Activity {
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		loadFromFile();
-		adapter = new ArrayAdapter<Tweet>(this,
-				R.layout.list_item, tweetlist);
+		String[] tweets = loadFromFile();
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private void loadFromFile() {
-
+	private String[] loadFromFile() {
+		ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-
-			Gson gson = new Gson();
-
-			//Taken https://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
-			// 2018-01-24
-
-			Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
-
-			tweetlist = gson.fromJson(in, listType);
+			String line = in.readLine();
+			while (line != null) {
+				tweets.add(line);
+				line = in.readLine();
+			}
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			tweetlist = new ArrayList<Tweet>();
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		return tweets.toArray(new String[tweets.size()]);
 	}
 	
-	private void saveInFile() {
+	private void saveInFile(String text, Date date) {
 		try {
 			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_PRIVATE);
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
-
-			Gson gson = new Gson();
-
-			gson.toJson(tweetlist, out);
-
-			out.flush();
-
+					Context.MODE_APPEND);
+			fos.write(new String(date.toString() + " | " + text)
+					.getBytes());
+			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -137,10 +104,5 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	protected void onDestroy(){
-		super.onDestroy();
-		Log.i("In Destroy method","The app is closing");
 	}
 }
