8f85632b3b2cb25f852947fac2adb2a5a84560b9
Echo-Gambino <nkb012pomm@gmail.com>
2018-02-28 17:51:38 -0700

    thing

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,19 +2,64 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
+/**
+ * Created by wz on 14/09/15.
+ */
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
     }
 
-    public
+    public void setup() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
 
     public void testStart() throws Exception {
         Activity activity = getActivity();
 
+    }
+
+    public void testClickTweetList() {
+        LonelyTwitterActivity activity = (LonelyTwitterActivity)solo.getCurrentActivity();
+
+        solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+
+        solo.enterText((EditText) solo.getView(R.id.body), "test Tweet");
+
+        solo.clickOnButton("Save");
+
+        solo.clearEditText((EditText) solo.getView(R.id.body));
+
+        assertTrue(solo.waitForText("Test Tweet!"));
+
+        final ListView oldTweetList = activity.getOldTweetsList();
+
+        Tweet tweet = (Tweet) oldTweetList.getItemAtPosition(0);
+
+        assertEquals("Test Tweet!", tweet.getDate());
+
+        solo.clickInList(0);
+
+        solo.assertCurrentActivity("Wrong Activity", EditTweetActivity.class);
+
+        solo.goBack();
+
+        solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -31,6 +31,9 @@ public class LonelyTwitterActivity extends Activity {
 	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
 	private ArrayAdapter<Tweet> adapter;
 
+	public ListView getOldTweetsList() {return oldTweetsList;}
+
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
